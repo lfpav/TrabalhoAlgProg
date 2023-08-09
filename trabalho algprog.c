3d011d4c843c14f92bc8ebd91aa07b7c8e18f68b
papapapo <42011905+papapapo@users.noreply.github.com>
2023-08-09 00:19:40 -0300

    reescrita da funcao de carregar mapa

--- trabalho algprog.c
+++ trabalho algprog.c
@@ -49,7 +49,7 @@ typedef struct Status_Jogo_t
     OBJETO_ESTATICO Bombas[MAX_OBJECTS];
     OBJETO_ESTATICO Armadilhas[MAX_OBJECTS];
     OBJETO_ESTATICO Portal;
-    float tempo_restante;
+    int tempo_restante;
     int mapaAtual;
     int InimigosNaFase;
 
@@ -64,6 +64,8 @@ Font fonteTitle;
 int tamanhotitle;
 bool GameStart = false;
 Texture2D sapo;
+time_t tempo_inicio;
+int tempo_atual;
 
 
 /* A funcao SalvaJogo recebe o ponteiro para o o struct STATUS atual, e armazena as informacoes relevantes em um arquivo com uma estrutura semelhante aos arquivos de mapa,
@@ -82,30 +84,14 @@ void SalvaJogo(STATUS *s)
 
         }
     }
-    fprintf(savegame,"%d\n%d\n%.2f\n%d",s->player.HP,s->player.bombAmount,s->tempo_restante,s->mapaAtual);
+    s->tempo_restante = tempo_atual;
+    fprintf(savegame,"%d\n%d\n%d\n%d",s->player.HP,s->player.bombAmount,s->tempo_restante,s->mapaAtual);
     fclose(savegame);
 
 
 
 }
-/* A funcao Carregajogo recebe o ponteiro para o struct STATUS atual, abre o arquivo JogoSalvo.txt, le as informacoes relevantes para o jogo e as armazena no struct */
-void CarregaJogo(STATUS *s)
-{
-    FILE* savegame;
-    savegame = fopen("JogoSalvo.txt","r");
-    for(int i=0;i<MAPLINES;i++)
-    {
-        for(int j=0;j<MAPCOLUMNS+1;j++)
-        {
-            s->CurrentLevelMatrix[i][j]=fgetc(savegame);
-        }
 
-    }
-    fscanf(savegame,"%d\n%d\n%.2f\n%d",&s->player.HP,&s->player.bombAmount,&s->tempo_restante,&s->mapaAtual);
-    fclose(savegame);
-    GameStart = true;
-
-}
 
 
 
@@ -148,46 +134,58 @@ void JogoRenderer(STATUS *s)
         }
     }
     DrawRectangle(0,450,900,300,RED);
+    for(int k=0;k<s->InimigosNaFase;k++)
+    {
+        DrawRectangleV(Vector2Scale(s->Inimigos[k].posInimigo,15),(Vector2){30,30},RED);
+    }
     //DrawTextEx()
 
 }
-/* CarregaMapa recebe o numero da fase desejada e carrega o arquivo correspondente ao nivel desejado.
+/* CarregaMapa recebe o numero da fase desejada e o tipo de carregamento carrega o arquivo correspondente ao nivel desejado.
+Caso seja tipo 0, a funcao carrega o mapa normalmente.
+Caso seja tipo 1, a funcao carrega o mapa baseado no arquivo JogoSalvo.txt
 A funcao usa Textformat junta o numero do level com mapa e .txt para carregar o mapa, substituindo a necessidade de diversas concatenacoes de strings e sprintf
 O arquivo de level precisa comecar com mapa, ter letra minuscula, ter um int depois do mapa e ser um arquivo txt
 Esse arquivo precisa ter MAPLINES linhas e MAPCOLUMNS colunas para a funcao funcionar direito
 A partir do arquivo determina a posicao inicial do jogador, inimigos e outros elementos do mapa, armazenando essas informacoes em uma matriz
 com MAPLINES linhas e MAPCOLUMNS colunas
 */
-void CarregaMapa(int levelNumber, char CurrentLevelMatrix[MAPLINES][MAPCOLUMNS],PLAYER *player, INIMIGO Inimigos[],int *InimigosNaFase)
+//void CarregaMapa(int levelNumber, char CurrentLevelMatrix[MAPLINES][MAPCOLUMNS],PLAYER *player, INIMIGO Inimigos[],int *InimigosNaFase)
+void CarregaMapa(STATUS *s,int type)
 {
-    char *levelName = TextFormat("mapa%d.txt",levelNumber);
+
+    char*levelName = TextFormat("mapa%d.txt",s->mapaAtual);
     int contadorInimigos = 0;
-    puts(levelName);
     //teste !!!!!!!!
     FILE* mapaLevel = fopen(levelName,"r");
-    *InimigosNaFase=0;
+    if(type==1)
+    {
+        fclose(mapaLevel);
+        mapaLevel=fopen("JogoSalvo.txt","r");
+    }
+    s->InimigosNaFase=0;
     for(int iMap=0; iMap<MAPLINES; iMap++)
     {
         for(int jMap=0; jMap<MAPCOLUMNS+1; jMap++)
         {
 
-            CurrentLevelMatrix[iMap][jMap] = fgetc(mapaLevel);
+            s->CurrentLevelMatrix[iMap][jMap] = fgetc(mapaLevel);
 
-            switch(CurrentLevelMatrix[iMap][jMap])
+            switch(s->CurrentLevelMatrix[iMap][jMap])
             {
             case 'J':
-                player->posplayer.x = jMap;
-                player->posplayer.y = iMap;
+                s->player.posplayer.x = jMap;
+                s->player.posplayer.y = iMap;
                 break;
             case 'I':
-                if(*InimigosNaFase<=MAX_INIMIGOS)
+                if(s->InimigosNaFase<=MAX_INIMIGOS)
                 {
-                    Inimigos[*InimigosNaFase].posInimigo.x = jMap;
-                    Inimigos[*InimigosNaFase].posInimigo.y = iMap;
-                    Inimigos[*InimigosNaFase].tipoInimigo = 1;
-                    Inimigos[*InimigosNaFase].HP = 5;
-                    Inimigos[*InimigosNaFase].ativo = true;
-                    *InimigosNaFase+=1;
+                    s->Inimigos[s->InimigosNaFase].posInimigo.x = jMap;
+                    s->Inimigos[s->InimigosNaFase].posInimigo.y = iMap;
+                    s->Inimigos[s->InimigosNaFase].tipoInimigo = 1;
+                    s->Inimigos[s->InimigosNaFase].HP = 5;
+                    s->Inimigos[s->InimigosNaFase].ativo = true;
+                    s->InimigosNaFase+=1;
                     break;
                 }
             default:
@@ -198,14 +196,19 @@ void CarregaMapa(int levelNumber, char CurrentLevelMatrix[MAPLINES][MAPCOLUMNS],
 
     }
     //desativa os inimigos excedentes ao numero de inimigos na fase
-    for (int i = *InimigosNaFase+1; i<MAX_INIMIGOS; i++)
+    for (int i = s->InimigosNaFase+1; i<MAX_INIMIGOS; i++)
     {
-        Inimigos[i].ativo = false;
+        s->Inimigos[i].ativo = false;
+
+    }
+    if(type==1)
+    {
+            fscanf(mapaLevel,"%d\n%d\n%d\n%d\n%d",&s->player.HP,&s->player.bombAmount,&s->tempo_restante,&s->mapaAtual);
 
     }
     //printf("%f", player->posplayer.x);
     fclose(mapaLevel);
-    return;
+
 
 }
 
@@ -219,9 +222,17 @@ void NovoJogo(STATUS *s)
     s->player.bombAmount=3;
     s->player.dmg = 1;
     s->mapaAtual=1;
-    CarregaMapa(s->mapaAtual,s->CurrentLevelMatrix,&s->player,s->Inimigos,&s->InimigosNaFase);
+    s->tempo_restante=0;
+    CarregaMapa(s,0);
     GameStart = true;
-    return;
+
+
+}
+/* A funcao Carregajogo recebe o ponteiro para o struct STATUS atual, e chama a funcao CarregaMapa com o tipo 1 para abrir o arquivo JogoSalvo.txt, le as informacoes relevantes para o jogo e as armazena no struct */
+void CarregaJogo(STATUS *s)
+{
+    CarregaMapa(s,1);
+    GameStart = true;
 
 }
 
@@ -281,8 +292,7 @@ int main()
     //CarregaMapa(1,CurrentLevelMatrix,&jogador,Inimigos,&InimigosNaFase);
     printf("%d",status_jogo_atual.InimigosNaFase);
     printf(" \n %d",tamanhotitle);
-    time_t tempo_inicio =time(NULL);
-    int tempoAtual;
+
 
     while(!WindowShouldClose())
     {
@@ -291,6 +301,7 @@ int main()
             BeginDrawing();
             ClearBackground(WHITE);
             TitleScreen();
+            tempo_inicio=time(NULL);
             EndDrawing();
 
          }
@@ -298,11 +309,14 @@ int main()
 
         BeginDrawing();
         ClearBackground(WHITE);
-        tempoAtual = time(NULL)-tempo_inicio;
+        tempo_atual = time(NULL)-tempo_inicio+status_jogo_atual.tempo_restante;
         JogoRenderer(&status_jogo_atual);
-        DrawText(TextFormat("Tempo:%d s",tempoAtual),450,450,50,BLACK);
-        DrawText(TextFormat("Vida:%d",status_jogo_atual.player.HP),450,550,50,BLACK);
-        DrawText(TextFormat("Bombas:%d",status_jogo_atual.player.bombAmount),450,500,50,BLACK);
+        DrawText(TextFormat("Tempo:%d s",tempo_atual),0,500,50,BLACK);
+        DrawText(TextFormat("Vida:%d",status_jogo_atual.player.HP),0,550,50,BLACK);
+        DrawText(TextFormat("Bombas:%d",status_jogo_atual.player.bombAmount),0,600,50,BLACK);
+        DrawText(TextFormat("Mapa atual:%d",status_jogo_atual.mapaAtual),0,450,50,BLACK);
+        DrawText(TextFormat("Inimigos:%d",status_jogo_atual.InimigosNaFase),0,700,50,BLACK);
+
 
         EndDrawing();
 
